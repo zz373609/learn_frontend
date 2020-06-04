41c481a23f789a1b5d7faac456c3e08b6459a45b
zhiqiang <zz373609@ohio.edu>
2020-06-04 13:25:43 +0800

    don

--- js/作用域/code.js
+++ js/作用域/code.js
@@ -1,3 +1,5 @@
+// -----------------------
+
 // function a() {
 //   var c= 100;
 //   b()
@@ -9,14 +11,56 @@
 
 // a()
 
-var c= 100;
-function a() {
+// -----------------------
 
-  function b(){
-    console.log(c)
-  }
-  b()
+// var c= 100;
+// function a() {
+
+//   function b(){
+//     console.log(c)
+//   }
+//   b()
+// }
+
+
+// a()
+
+
+// -----------------------
+
+// function a(){
+//   var c = 100;
+//   return function b(){
+//     console.log(c)
+//   }
+// }
+// var h = a()
+// console.log(h())
+
+
+// -----------------
+
+// function n(){
+//   console.log(this())
+// }
+// n()
+
+// console.log(Function)
+
+Function.prototype.call2 = function(context){
+  //获取调用call2的函数，用this
+  context.fn = this;
+  context.fn();
+  delete context.fn;
+
+}
+// 测试一下
+var foo = {
+    value: 1
+};
+
+function bar() {
+    console.log(this.value);
 }
 
-
-a()
+bar.call2(foo); // 1
